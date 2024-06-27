d2675989593fc44a01636f69a080287da0fbd6c9
Prarthanabalekkala <prarthanarb2@gmail.com>
2024-06-27 10:20:07 +0530

    updates done by...

--- script.js
+++ script.js
@@ -28,9 +28,54 @@ const x=null;
 var y;
 
 console.log(typeof name);
-
-
 console.log('My name is '+ name +' and my age is '+ age);
+
+
+
+
+//array
+
+
+arrayOne=["john",30,true,null,undefined]
+console.log(arrayOne);
+
+
+arrayOne=["john",30,true,null,undefined]
+console.log(arrayOne);
+
+//pushs
+arrayOne.push("apple");
+console.log(arrayOne);
+
+//pop
+arrayOne.pop();
+console.log(arrayOne);
+
+arrayOne.unshift("mango");
+console.log(arrayOne);
+
+//length
+console.log(arrayOne.length);
+
+arrayOne.splice(3,1);
+console.log(arrayOne);
+
+let valueOnee="2";
+let valueTwoo=2;
+
+console.log(valueOnee==valueTwoo);
+console.log(valueOnee===valueTwoo);
+
+
+let value=16;
+if (value>=18)
+{
+    console.log("u are adult");
+}
+else{
+    console.log("minor");
+}
+
 
 
 
